6c742c94c3d9415057adea6ed249c907fbe5f729
Per Berge <berge.per2493@gmail.com>
2023-02-06 18:43:52 +0100

    added animations

---
+++ src/ts/helpers/animations.ts
@@ -0,0 +1,27 @@
+export const titleAni = () => {
+  let reveals = document.querySelectorAll(".reveal--left");
+  for (let i = 0; i < reveals.length; i++) {
+    let windowHeight = window.innerHeight;
+    let elementTop = reveals[i].getBoundingClientRect().top;
+    let elementVisible = 150;
+    if (elementTop < windowHeight - elementVisible) {
+      reveals[i].classList.add("active");
+    } else {
+      reveals[i].classList.remove("active");
+    }
+  }
+};
+
+export const contentAni = () => {
+  let reveals = document.querySelectorAll(".reveal--right");
+  for (let i = 0; i < reveals.length; i++) {
+    let windowHeight = window.innerHeight;
+    let elementTop = reveals[i].getBoundingClientRect().top;
+    let elementVisible = 150;
+    if (elementTop < windowHeight - elementVisible) {
+      reveals[i].classList.add("active");
+    } else {
+      reveals[i].classList.remove("active");
+    }
+  }
+};
--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,3 +1,7 @@
+import { contentAni, titleAni } from "./helpers/animations";
 import { gitrepo } from "./services/GitRepo";
 
 gitrepo();
+
+window.addEventListener("scroll", titleAni);
+window.addEventListener("scroll", contentAni);
